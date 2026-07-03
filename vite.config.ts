d64bdcc0f077e3528045cb77b5a87858e8bb75d2
CYF <873401580@qq.com>
2026-07-03 19:56:57 +0800

    跨域配置

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,15 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
 
-  //端口修改
+  //端口修改。 选项参考：https://vitejs.cn/config/server-options
   server: {
     port: 7070,
     host: "0.0.0.0",
-    proxy:{
-
+    proxy: {
+      '/api': {  // 以 /api 前缀的请求，都会走代理。
+        target: "http://localhost:9090", //代理目标：后端服务器地址
+        changeOrigin: true
+      }
     }
   }
 })
